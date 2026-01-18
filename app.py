faa7e3449e969175bee457ad071a2c9916ae6c99
Vixelboy <tawakal618@gmail.com>
2026-01-18 12:51:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
 import streamlit as st
 from groq import Groq
 
-st.markdown("""
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    """, unsafe_allow_html=True)
-
 st.set_page_config(page_title="Guru TKJ AI", page_icon="💻")
-st.title("🤖 Kelas Digital Pak Guru TKJ")
-st.caption("Materi: Jaringan, Mikrotik, Cybersecurity, Coding, dll")
+st.title("Digital Agent TKJ")
+st.caption("Materi: Jaringan, Mikrotik, Cybersecurity, & Coding")
 
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+client = Groq(api_key="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")
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "system", "content": "Anda adalah Pak Guru TKJ yang ahli dan ramah. Gunakan bahasa SMK dan analogi jaringan."}
+        {"role": "system", "content": "Anda adalah Pak Guru TKJ yang ahli dan ramah. Gunakan analogi jaringan dalam menjelaskan."}
     ]
-
+    
 for message in st.session_state.messages:
     if message["role"] != "system":
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 if prompt := st.chat_input("Tanya apa hari ini, Nak?"):
+    # Tampilkan pesan siswa
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     try:
         response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="llama-3.1-8b-instant",
             messages=st.session_state.messages
         )
+        
         answer = response.choices[0].message.content
+        
         with st.chat_message("assistant"):
             st.markdown(answer)
+        
         st.session_state.messages.append({"role": "assistant", "content": answer})
     except Exception as e:
         st.error(f"Waduh, koneksi putus: {e}")
